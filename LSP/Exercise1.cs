d1f8b7f812b9e7b8ec6cc7dc7ee0e3a8354e9d62
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-19 18:01:35 +0300

    add Exercise1

---
+++ LSP/Exercise1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    internal interface ICanFly
+    {
+        public void Fly();
+    }
+
+    internal class Bird
+    {
+
+    }
+    internal class FlyBird : Bird, ICanFly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I can fly.");
+        }
+    }
+
+    internal class Penguin : Bird
+    {
+
+    }
+
+    internal static class Test1
+    {
+        public static void MakeBirdFly(ICanFly flyBird)
+        {
+            flyBird.Fly();
+        }
+    }
+
+}
--- LSP/Program.cs
+++ LSP/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ICanFly b = new FlyBird();
+            b.Fly();
+        }
+    }
+
+    
+
+
+}
